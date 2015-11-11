c4ed71f123916de6ba3409cbf0f1f19c8a29653d
Cesar Ilharco Magalhaes <cesarim22@gmail.com>
2015-11-11 20:48:34 +0200

    Client and ClientManager classes

---
+++ client.py
@@ -0,0 +1,15 @@
+class Client(object):
+
+	def __init__(self, id_, demand_):
+		self.id = id_
+		self.demand = demand_
+
+
+class ClientManager(object):
+
+	def __init__(self):
+		self.__latest_id = 0
+
+	def create_client(self, demand):
+		self.__latest_id += 1
+		return Client(self.__latest_id, demand)
