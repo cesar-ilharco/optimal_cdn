4aab0023426a6d6eca7ab390c3b15ed3a4f956b6
Cesar Ilharco Magalhaes <cesarim22@gmail.com>
2015-11-11 20:49:44 +0200

    PlacementManager class

---
+++ placement.py
@@ -0,0 +1,19 @@
+class PlacementManager(object):
+
+	def __init__ (self, servers_):
+		self.servers = servers_
+		self.__map_client_server = {}
+		self.__map_server_clients = {server : set() for server in self.servers}
+
+	def place_client(self, client, server):
+		server.used_capacity += client.demand
+		self.__map_client_server[client] = server
+		self.__map_server_clients[server].add(client)
+
+	def get_clients_served_by(self, server):
+		return self.__map_server_clients[server]
+
+	def get_server(self, client):
+		return self.__map_client_server[client]
+
+
