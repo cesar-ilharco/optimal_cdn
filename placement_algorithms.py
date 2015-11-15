5301ff457803d7a301009dcb98162f1537332ed9
davisilva15 <davisilva15@gmail.com>
2015-11-15 05:13:42 +0100

    Update placement_algorithms.py

--- placement_algorithms.py
+++ placement_algorithms.py
@@ -13,31 +13,34 @@ def placement(clients, servers):
 	return placement_manager
 
 
-def optimal_placement (placement_manager):
+def optimal_placement (clients, servers):
+	placement_manager = placement(clients, servers)
 	avg_used_capacity = sum(server.used_capacity for server in placement_manager.servers) / len(placement_manager.servers)
 	sorted_servers = sorted(placement_manager.servers)
-	indexes = []
 	max_used_capacity = sorted_servers[-1].used_capacity
 	index, next_server = __find_closest_used_capacity(sorted_servers, avg_used_capacity)
 	del sorted_servers[index]
-	indexes.append(next_server.id)
 	accumulated_capacity = avg_used_capacity + next_server.used_capacity - max_used_capacity
 	lambda1 = accumulated_capacity/next_server.used_capacity
-	placement_manager.set_multiplicative_factor(next_server, placement_manager.get_clients_served_by(next_server), lambda1)
+	first_clients = copy.copy(placement_manager.get_clients_served_by(next_server))
+	placement_manager.set_multiplicative_factor(next_server, first_clients, lambda1)
+	next_server.used_capacity = lambda1*sum(client.demand for client in first_clients)
 	lambda2 = 1.0 - lambda1
 	last_server = next_server
 	for count in range(1, len(placement_manager.servers)):
 		target = (count+2)*avg_used_capacity - max_used_capacity - accumulated_capacity
 		index, next_server = __find_closest_used_capacity(sorted_servers, target)
 		del sorted_servers[index]
-		indexes.append(next_server.id)
 		accumulated_capacity += next_server.used_capacity
 		lambda1 = (accumulated_capacity-count*avg_used_capacity)/next_server.used_capacity
 		placement_manager.set_multiplicative_factor(next_server, placement_manager.get_clients_served_by(next_server), lambda1)
+		next_server.used_capacity = lambda1*sum(client.demand for client in placement_manager.get_clients_served_by(next_server))
 		placement_manager.set_multiplicative_factor(last_server, placement_manager.get_clients_served_by(next_server), 1.0 - lambda1)
+		last_server.used_capacity += (1.0 - lambda1)*sum(client.demand for client in placement_manager.get_clients_served_by(next_server))
 		last_server = next_server
-	placement_manager.set_multiplicative_factor(last_server, placement_manager.get_clients_served_by(next_server), lambda2)
-	return placement_manager, indexes
+	placement_manager.set_multiplicative_factor(last_server, first_clients, lambda2)
+	last_server.used_capacity += lambda2*sum(client.demand for client in first_clients)
+	return placement_manager
 
 
 def __find_closest_used_capacity (servers, target):
