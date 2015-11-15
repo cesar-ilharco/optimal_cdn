9fc53245d5a9668d809497e2a2ad710d6a0441fb
davisilva15 <davisilva15@gmail.com>
2015-11-15 05:11:53 +0100

    Added get_multiplicative_factor functions

--- placement.py
+++ placement.py
@@ -16,6 +16,12 @@ class PlacementManager(object):
 	def get_servers(self, client):
 		return self.__map_client_servers[client]
 
+	def get_multiplicative_factor_from_server(self, server, client):
+		return self.__map_server_clients[server][client]
+
+	def get_multiplicative_factor_from_client(self, server, client):
+		return self.__map_server_clients[client][server]
+
 	def set_multiplicative_factor(self, server, clients, multiplicative_factor):
 		for client in clients:
 			if client not in self.__map_client_servers:
