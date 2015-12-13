b062deb2f65e75f4ea6a2f15592d833f0e86cfc0
davisilva15 <davisilva15@gmail.com>
2015-12-13 01:34:42 +0100

    Updated __find_closest_used_capacity

--- placement_algorithms.py
+++ placement_algorithms.py
@@ -28,26 +28,24 @@ def optimal_placement(clients, servers):
     avg_used_capacity = sum(server.used_capacity for server in placement_manager.servers) / len(placement_manager.servers)
     sorted_servers = sorted(placement_manager.servers)
     max_used_capacity = sorted_servers[-1].used_capacity
-    index, next_server = __find_closest_used_capacity(sorted_servers, avg_used_capacity)
+    index, next_server = __find_closest_used_capacity(sorted_servers, avg_used_capacity, True)
     del sorted_servers[index]
     accumulated_capacity = avg_used_capacity + next_server.used_capacity - max_used_capacity
-    lambda1 = accumulated_capacity/next_server.used_capacity
+    lambda1 = accumulated_capacity / next_server.used_capacity
+    lambda2 = 1.0 - lambda1
     next_clients = placement_manager.get_clients_served_by(next_server).keys()
     first_clients = copy.copy(list(next_clients))
-    placement_manager.reset(first_clients)
-    placement_manager.set_multiplicative_factor(next_server, first_clients, lambda1)
+    placement_manager.change_multiplicative_factor(next_server, lambda1)
     next_server.used_capacity = lambda1*sum(client.demand for client in first_clients)
-    lambda2 = 1.0 - lambda1
     last_server = next_server
     for count in range(1, len(placement_manager.servers)):
         target = (count+2)*avg_used_capacity - max_used_capacity - accumulated_capacity
-        index, next_server = __find_closest_used_capacity(sorted_servers, target)
-        next_clients = placement_manager.get_clients_served_by(next_server).keys()
+        index, next_server = __find_closest_used_capacity(sorted_servers, target, accumulated_capacity < (count+1)*avg_used_capacity - max_used_capacity)
         del sorted_servers[index]
+        next_clients = placement_manager.get_clients_served_by(next_server).keys()
         accumulated_capacity += next_server.used_capacity
-        lambda1 = (accumulated_capacity-count*avg_used_capacity)/next_server.used_capacity
-        placement_manager.reset(next_clients)
-        placement_manager.set_multiplicative_factor(next_server, next_clients, lambda1)
+        lambda1 = (accumulated_capacity - count*avg_used_capacity) / next_server.used_capacity
+        placement_manager.change_multiplicative_factor(next_server, lambda1)
         next_server.used_capacity = lambda1*sum(client.demand for client in next_clients)
         placement_manager.set_multiplicative_factor(last_server, next_clients, 1.0 - lambda1)
         last_server.used_capacity += (1.0 - lambda1)*sum(client.demand for client in next_clients)
@@ -57,10 +55,11 @@ def optimal_placement(clients, servers):
     return placement_manager
 
 
-def __find_closest_used_capacity(servers, target):
+def __find_closest_used_capacity(servers, target, bigger):
     """
-    Given a sorted list of servers, finds the server in that list whose used capacity is the closest to a given target.
-    Returns this server and its position in the list.
+    Given a sorted list of servers, finds the server in that list whose used capacity is the smallest amongst those bigger
+    than a given target (or biggest amongst those smaller than the target).
+    Returns this server and its position on the list.
     """
     if len(servers) == 1:
         return [0, servers[0]]
@@ -71,12 +70,17 @@ def __find_closest_used_capacity(servers, target):
     if servers[len_med].used_capacity == target:
         return closest
     if servers[len_med].used_capacity > target:
-        temp = __find_closest_used_capacity(servers[:len_med], target)
-        if abs(temp[1].used_capacity - target) < closest[1].used_capacity - target:
-            return temp
-        return closest
-    temp = __find_closest_used_capacity(servers[len_med+1:], target)
+        temp = __find_closest_used_capacity(servers[:len_med], target, bigger)
+        if bigger:
+            if temp[1].used_capacity >= target:
+                return temp
+            return closest
+        return temp
+    temp = __find_closest_used_capacity(servers[len_med+1:], target, bigger)
     temp[0] += len_med + 1
-    if abs(temp[1].used_capacity - target) < target - closest[1].used_capacity:
+    if bigger:
+        return temp
+    if temp[1].used_capacity <= target:
         return temp
     return closest
+    
